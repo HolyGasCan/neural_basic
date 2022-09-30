9a8962c676593c85d866a445cdc575f66da2e2d1
HolyGasCan <bydrosych@mail.ru>
2022-09-30 11:07:28 +0300

    final changes with all the necessary comments

--- main.py
+++ main.py
@@ -3,70 +3,36 @@ from matplotlib import pyplot as plt
 import threading
 import time
 
-from prepare_data import PreparationData
-from neural import Neural, get_accuracy
+from neural import Neural
 
+# offset for conversion of characters into UNICODE and vice versa
 UNICODE_OFFSET = 1072
 
-def preparation_data():
-    # data_path = './data'
-    # prepare_data = PreparationData(data_path)
-    # prepare_data.rename_files('pic')
-    # prepare_data.create_dataset()
-    # matrix = prepare_data.create_matrix()
+
+# numpy dataset load
+def prepare_data():
     matrix = np.load('data.npy')
-    # print(np.linalg.matrix_rank(matrix))
-    # draw(matrix, 30)
     return matrix
 
 
-def draw(matrix, index):
-    current_image = matrix[index]
-    # label = chr(int(current_image[0]))
-    current_image = current_image[1:169]
-    current_image = current_image.reshape((14, 12)) * 255
-    plt.imshow(current_image, interpolation='nearest', cmap='gray')
-    plt.show()
-
-
-def create_neural(data, neurons, it, accuracy, index=201):
+# main NN function
+def create_neural(data, neurons, it, accuracy):
+    # creation of NN
     neural = Neural(data, neurons)
-    current_image = data[index + 2000]
-    label = chr(int(current_image[0] + UNICODE_OFFSET))
+
+    # gradient descent method to teach the NN
     neural.W1, neural.b1, neural.W2, neural.b2 = neural.gradient_descent(neural.x_train, neural.y_train, 0.10, it)
-    save_neural(neural)
-    # load_neural(neural)
+
+    # accuracy check of the trained NN
     cnt = 0
     for i in range(2000):
         prediction = neural.make_predictions(neural.x_dev[:, i, None])
         if prediction != neural.y_dev[i]:
-            # print(prediction, neural.y_dev[i])
             cnt += 1
-            # print(i)
-    prediction = neural.make_predictions(neural.x_train[:, index, None])
-    # print("Prediction: ", chr(int(prediction + UNICODE_OFFSET)))
-    # print("Label: ", label)
-    #
-    # current_image = current_image[1:169]
-    # current_image = current_image.reshape((14, 12)) * 255
-    # plt.imshow(current_image, interpolation='nearest', cmap='gray')
-    # plt.show()
-
-    # save_neural(neural)
-    #
-    # neural = Neural(data)
-    # load_neural(neural)
-    # prediction = neural.make_predictions(neural.x_train[:, index, None])
-    # print("Prediction: ", chr(int(prediction + UNICODE_OFFSET)))
-    # print("Label: ", label)
-
-    # current_image = current_image[1:169]
-    # current_image = current_image.reshape((14, 12)) * 255
-    # plt.imshow(current_image, interpolation='nearest', cmap='gray')
-    # plt.show()
     accuracy.append((1 - cnt / 2000) * 100)
 
 
+# NN save
 def save_neural(neural):
     np.save('W1', neural.W1)
     np.save('W2', neural.W2)
@@ -74,34 +40,55 @@ def save_neural(neural):
     np.save('b2', neural.b2)
 
 
+# NN load
 def load_neural(neural):
     neural.W1 = np.load('W1.npy')
     neural.W2 = np.load('W2.npy')
     neural.b1 = np.load('b1.npy')
     neural.b2 = np.load('b2.npy')
 
-# UNICODE - это моя жизнь (44F=Я, 450=???, 451=Ё)
-if __name__ == '__main__':
+
+# main NN function
+def main():
+    # initial time stamp for convenience
     start_time = time.time()
 
-    data = preparation_data()
-
+    # NN creation, teaching and accuracy check
+    data = prepare_data()
     threads = list()
+
+    # list of amount of neurons of NN to check accuracy on various number of iterations
     neurons = [5, 10, 25, 35, 50]
+
+    # main loop for NN
     for neuron in neurons:
         accuracy = list()
         iterations = list()
-        for i in range(25, 401, 25):
+        for i in range(25, 301, 25):
+            # creation of multiple threads for each amount of iterations
             x = threading.Thread(target=create_neural, args=(data, neuron, i, accuracy))
             threads.append(x)
             x.start()
             iterations.append(i)
         for j in range(len(threads)):
+            # waiting for all the threads to finish their iteration
             threads[j].join()
+        # plotting the results of NN accuracy check with specific amount of neurons
         plt.plot(iterations, accuracy, label=f"{neuron} neurons")
+
+    # time stamp after all the NN's accuracies are checked
     elapsed_time = round(time.time() - start_time, 2)
+
+    # time taken to do the work
     print("Time elapsed: " + str(int(elapsed_time // 60)) + " minutes and "
           + str(int(elapsed_time - 60 * (elapsed_time // 60))) + " seconds")
 
+    # show plot
     plt.legend()
     plt.show()
+    return None
+
+
+# main check
+if __name__ == '__main__':
+    main()
--- neural.py
+++ neural.py
@@ -1,46 +1,43 @@
-import numpy
 import numpy as np
 import copy
 
+# width and height of images in dataset
 WIDTH = 12
 HEIGHT = 14
 
-NUM_OF_ALPHABET = 33 + 1   # UNICODE - лучшее изобретение человечества
+# number of distinct letters
+NUM_OF_ALPHABET = 33 + 1  # UNICODE is the best invention the mankind has for russian letters *thumbs_up*
 
-B = 0.50
-
+# seed for creating initial weights
 np.random.seed(1337228)
 
 
+# generation of initial weights and biases
 def gen_params(NUM_OF_NEURONS):
     W1 = np.random.normal(size=(NUM_OF_NEURONS, WIDTH * HEIGHT)) * np.sqrt(1. / (WIDTH * HEIGHT))
     b1 = np.random.normal(size=(NUM_OF_NEURONS, 1)) * np.sqrt(1. / NUM_OF_NEURONS)
     W2 = np.random.normal(size=(NUM_OF_ALPHABET, NUM_OF_NEURONS)) * np.sqrt(1. / (NUM_OF_NEURONS * 2))
     b2 = np.random.normal(size=(NUM_OF_ALPHABET, 1)) * np.sqrt(1. / (WIDTH * HEIGHT))
-    # W1 = np.random.rand(NUM_OF_NEURONS, WIDTH * HEIGHT) - B
-    # W2 = np.random.rand(NUM_OF_ALPHABET, NUM_OF_NEURONS) - B
-    # b1 = np.random.rand(NUM_OF_NEURONS, 1) - B
-    # b2 = np.random.rand(NUM_OF_ALPHABET, 1) - B
-    # np.save('W1_init', W1)
-    # np.save('b1_init', b1)
-    # np.save('W2_init', W2)
-    # np.save('b2_init', b2)
     return W1, b1, W2, b2
 
 
+# first activation function
+def relu(z):
+    return np.maximum(z, 0)
+
+
+# derivative of relu
+def relu_deriv(z):
+    return z > 0
+
+
+# second activation function
 def softmax(z):
     a = np.exp(z) / sum(np.exp(z))
     return a
 
 
-def relu(z):
-    return np.maximum(z, 0)
-
-
-def relu_deriv(z):
-    return z > 0
-
-
+# yep, that's a function, yep-yep
 def one_hot(y):
     one_hot_y = np.zeros((y.size, y.max() + 1))
     one_hot_y[np.arange(y.size), y] = 1
@@ -48,37 +45,38 @@ def one_hot(y):
     return one_hot_y
 
 
+# getting all the predictions while teaching the NN
 def get_predictions(a2):
     return np.argmax(a2, 0)
 
 
+# calculating the accuracy of NN based on its predictions
 def get_accuracy(predictions, y):
     print(predictions, y)
     return np.sum(predictions == y) / y.size
 
 
+# main NN class
 class Neural:
     def __init__(self, data, NUM_OF_NEURONS):
+        # initial parameters
         self.W1, self.b1, self.W2, self.b2 = gen_params(NUM_OF_NEURONS)
-        # np.random.shuffle(data)
-        # np.save('data', data)
 
+        # dimensions of the dataset
         self.m, self.n = data.shape
 
-        # self.W1 = np.load('W1_init.npy')
-        # self.W2 = np.load('W2_init.npy')
-        # self.b1 = np.load('b1_init.npy')
-        # self.b2 = np.load('b2_init.npy')
-
+        # reservation of first 2000 entries of dataset for accuracy checking
         self.data_dev = data[0:2000].T
         self.y_dev = self.data_dev[0]
         self.x_dev = self.data_dev[1:self.n]
 
+        # taking the rest of the dataset for teaching purposes
         self.data_train = data[2000:self.m].T
         y_train = self.data_train[0]
         self.y_train = y_train.astype(int)
         self.x_train = self.data_train[1:self.n]
 
+    # forward propagation method
     def forward_prop(self, x):
         z1 = self.W1.dot(x) + self.b1
         a1 = relu(z1)
@@ -86,6 +84,7 @@ class Neural:
         a2 = softmax(z2)
         return z1, a1, z2, a2
 
+    # backward propagation method
     def backward_prop(self, z1, a1, z2, a2, x, y):
         one_hot_y = one_hot(y)
         dz2 = a2 - one_hot_y
@@ -97,33 +96,29 @@ class Neural:
         db1 = 1 / self.m * np.sum(dz1)
         return dW1, db1, dW2, db2
 
+    # updating parameters after forward and backward propagations
     def update_params(self, dW1, db1, dW2, db2, alpha):
         self.W1 = self.W1 - alpha * dW1
         self.b1 = self.b1 - alpha * db1
         self.W2 = self.W2 - alpha * dW2
         self.b2 = self.b2 - alpha * db2
 
+    # gradient descent method
     def gradient_descent(self, x, y, alpha, iterations):
         for i in range(iterations):
             z1, a1, z2, a2 = self.forward_prop(x)
             dW1, db1, dW2, db2 = self.backward_prop(z1, a1, z2, a2, x, y)
             self.update_params(dW1, db1, dW2, db2, alpha)
+
+            # checking accuracy every 10 iterations
             if i % 10 == 0:
                 print("Iteration: ", i)
                 predictions = get_predictions(a2)
                 print(get_accuracy(predictions, y))
         return self.W1, self.b1, self.W2, self.b2
 
+    # getting predictions of NN
     def make_predictions(self, X):
         _, _, _, a2 = self.forward_prop(X)
         predictions = get_predictions(a2)
         return predictions
-
-    '''
-    def test_prediction(self, index):
-        current_image = self.x_train[:, index, None]
-        prediction = self.make_predictions(self.x_train[:, index, None])
-        label = self.y_train[index]
-        print("Prediction: ", prediction)
-        print("Label: ", label)
-    '''
